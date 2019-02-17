c3372fb53551c58700633f1bbf570880c2fb8897
ramnareshvarma <ramnaresh.453@gmail.com>
2019-02-17 12:07:21 -0800

    check if no. is prime or not

--- myfile3.py
+++ myfile3.py
@@ -1,19 +1,22 @@
-# Python program to find the factorial of a number provided by the user.
+# Python program to check if the input number is prime or not
 
-# change the value for a different result
-num = 7
+num = 407
 
-# uncomment to take input from the user
-#num = int(input("Enter a number: "))
+# take input from the user
+# num = int(input("Enter a number: "))
 
-factorial = 1
-
-# check if the number is negative, positive or zero
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
+# prime numbers are greater than 1
+if num > 1:
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
+       
+# if input number is less than
+# or equal to 1, it is not prime
 else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+   print(num,"is not a prime number")
